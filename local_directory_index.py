d79fb8f9f1d91678b6a8bd7d986f966e513564ff
Daniel Dreier <d@puppet.com>
2017-05-09 14:04:12 -0700

    working local directory index

---
+++ local_directory_index.py
@@ -0,0 +1,115 @@
+#from __future__ import print_function
+import jinja2
+from jinja2 import Environment
+#import re
+#import collections
+#from timeit import default_timer as timer # performance profiling
+import os,sys
+import datetime
+
+# Python 2/3 compatible hack
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+def index_link(prefix, current_order_by, new_order_by, reverse_order):
+    if current_order_by == new_order_by:
+        return index_file_name(prefix, current_order_by, not reverse_order)
+    else:
+        return index_file_name(prefix, new_order_by, False)
+
+# use a templating library to turn a prefix and a list of contents into an HTML directory index
+def render_index(prefix, order_by, contents, reverse_order):
+    parent_directory = '/'.join(prefix.split('/')[:-1])
+
+    index_by = {}
+    index_by['name'] = index_link(prefix, order_by, 'name', reverse_order)
+    index_by['size'] = index_link(prefix, order_by, 'size', reverse_order)
+    index_by['lastModified'] = index_link(prefix, order_by, 'lastModified', reverse_order)
+
+    sorted_contents = sorted(contents, key=lambda k: k[order_by], reverse=reverse_order)
+
+    HTML = """
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+    <html>
+     <head>
+       <title>Index of /{{prefix}}</title>
+     </head>
+     <body>
+       <h1>Index of /{{prefix}}</h1>
+    <table><tr><th></th><th><a href="{{index_link['name']}}">Name</a></th><th><a href="{{index_link['lastModified']}}">Last modified</a></th><th><a href="{{index_link['size']}}">Size</a></th><th>Description</th></tr><tr><th colspan="5"><hr></th></tr>
+    {% if prefix %}
+    <tr><td valign="top"><img src="https://s3-us-west-2.amazonaws.com/icons.puppet.com/back.gif"></td><td><a href="/{{parent_directory}}">Parent Directory</a></td><td>&nbsp;</td><td align="right">  - </td><td>&nbsp;</td></tr>
+    {% endif %}
+    {% for item in contents %}
+        <tr><td valign="top"><img src="https://s3-us-west-2.amazonaws.com/icons.puppet.com/{{item['icon']}}" alt="[DIR]"></td><td><a href="{{item['name'].split('/')[-1:][0]}}">{{item['name'].split('/')[-1:][0]}}</a></td><td align="right">{{item['lastModified']}}  </td><td align="right"> {{item['size']}}</td><td>&nbsp;</td></tr>
+    {% endfor %}
+    <tr><th colspan="5"><hr></th></tr>
+    </table>
+    </body></html>
+    """
+
+    return Environment().from_string(HTML).render(prefix=prefix, contents=sorted_contents, parent_directory=parent_directory, index_link=index_by)
+
+def index_file_name(prefix, order_by, reverse_order):
+    order_suffix = "_reverse" if reverse_order else ""
+    file_name = 'index' + '_by_' + order_by + order_suffix + '.html'
+    return file_name if len(prefix) == 0 else prefix + '/' + file_name
+
+# Reorganization
+
+# Step 2: generate, render indexes
+
+sys.setrecursionlimit(2100000000)
+
+def format_date(d):
+    return datetime.datetime.fromtimestamp(
+        int(d)
+    ).strftime('%Y-%m-%d %H:%M:%S')
+
+def format_size(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
+def generate_indexes():
+    indexes_to_generate = []
+    for order_by in ['name', 'size', 'lastModified']:
+        for reverse_order in [True, False]:
+            indexes_to_generate.append({'prefix': prefix, 'contents': contents, 'order_by': order_by, 'reverse_order': reverse_order})
+
+def walk(path):
+    contents = os.listdir(path)
+    directory_listing = []
+    # generate indexes for current path
+    for file in contents:
+        # add size, lastModified, file/folder type to metadata
+        fullpath = os.path.join(path, path)
+        directory_listing.append({
+            'name': file,
+            'lastModified': os.path.getmtime(fullpath),
+            'size': os.path.getsize(fullpath)
+        })
+    for order_by in ['name', 'size', 'lastModified']:
+        for reverse_order in [True, False]:
+            file_name = os.path.join(path, index_file_name('', order_by, reverse_order))
+            rendered_html = render_index(path, order_by, directory_listing, reverse_order)
+            #print(file_name)
+            index_file = open(file_name, 'w')
+            index_file.write(rendered_html)
+            index_file.close()
+
+    # recurse into subdirectories
+    for file in contents:
+        file_pwd = os.path.join(path, file)
+        if os.path.isdir(file_pwd):
+            walk(file_pwd)
+
+walk('/Users/daniel/Downloads/weth/yum')
