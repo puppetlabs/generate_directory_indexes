fd84aeb738b7ac55cec2ed2025a1dc7153cb31da
Daniel Dreier <d@puppet.com>
2017-05-10 11:42:45 -0700

    clean up repository

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp -R lib/python2.7/site-packages/* ./build
-cp lambda_function.py ./build
-cd build
-zip -r ../generate_directory_indexes.zip ./*
-aws lambda update-function-code --function generate_directory_indexes --zip-file fileb://../generate_directory_indexes.zip --publish
