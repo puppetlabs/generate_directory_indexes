77016bf2ca7a574c54f589a09574444c4455ae36
Daniel Dreier <d@puppet.com>
2017-05-10 11:07:39 -0700

    add incremental verbosity

--- local_directory_index.py
+++ local_directory_index.py
@@ -1,19 +1,11 @@
+#!/usr/bin/env python
 import jinja2
 from jinja2 import Environment
 import os,sys
 import datetime
 import argparse
 import re
-
-#sys.setrecursionlimit(2100000000)
-
-parser = argparse.ArgumentParser()
-parser.add_argument("path", help="Path to index and write indexes to")
-parser.add_argument("--test", help="Only print files to be created without writing them to disk", action="store_true")
-parser.add_argument("--verbose", help="Print names of files as they're created", action="store_true")
-args = parser.parse_args()
-print(args.path)
-
+import logging
 
 # Python 2/3 compatible hack
 try:
@@ -21,9 +13,30 @@ try:
 except ImportError:
     from io import StringIO
 
-import logging
+#sys.setrecursionlimit(2100000000)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="Path to index and write indexes to")
+parser.add_argument("--noop", help="Only print files to be created without writing them to disk", action="store_true")
+parser.add_argument('--verbose', '-v', help="Verbose output. Repeat (up to -vvv) for more verbosity", action='count')
+args = parser.parse_args()
+
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+if args.verbose == None:
+    logging.basicConfig(level=logging.ERROR)
+if args.verbose == 1:
+    logging.basicConfig(level=logging.WARNING)
+elif args.verbose == 2:
+    logging.basicConfig(level=logging.INFO)
+elif args.verbose >= 3:
+    logging.basicConfig(level=logging.DEBUG)
+
+logger.info("info")
+logger.error("error")
+logger.warning("warning")
+logger.debug("debug")
+
+exit()
 
 def index_link(prefix, current_order_by, new_order_by, reverse_order):
     if current_order_by == new_order_by:
@@ -41,6 +54,7 @@ def render_index(prefix, order_by, contents, reverse_order):
     index_by['lastModified'] = index_link(prefix, order_by, 'lastModified', reverse_order)
 
     sorted_contents = sorted(contents, key=lambda k: k[order_by], reverse=reverse_order)
+    formatted_contents = format_directory_listing(sorted_contents)
 
     HTML = """
     <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
@@ -52,7 +66,7 @@ def render_index(prefix, order_by, contents, reverse_order):
        <h1>Index of /{{prefix}}</h1>
     <table><tr><th></th><th><a href="{{index_link['name']}}">Name</a></th><th><a href="{{index_link['lastModified']}}">Last modified</a></th><th><a href="{{index_link['size']}}">Size</a></th><th>Description</th></tr><tr><th colspan="5"><hr></th></tr>
     {% if prefix %}
-    <tr><td valign="top"><img src="https://s3-us-west-2.amazonaws.com/icons.puppet.com/back.gif"></td><td><a href="/{{parent_directory}}">Parent Directory</a></td><td>&nbsp;</td><td align="right">  - </td><td>&nbsp;</td></tr>
+    <tr><td valign="top"><img src="https://s3-us-west-2.amazonaws.com/icons.puppet.com/back.gif"></td><td><a href="/{{parent_directory}}/index_by_name.html">Parent Directory</a></td><td>&nbsp;</td><td align="right">  - </td><td>&nbsp;</td></tr>
     {% endif %}
     {% for item in contents %}
         <tr><td valign="top"><img src="https://s3-us-west-2.amazonaws.com/icons.puppet.com/{{item['icon']}}" alt="[DIR]"></td><td><a href="{{item['name'].split('/')[-1:][0]}}">{{item['name'].split('/')[-1:][0]}}</a></td><td align="right">{{item['lastModified']}}  </td><td align="right"> {{item['size']}}</td><td>&nbsp;</td></tr>
@@ -62,7 +76,7 @@ def render_index(prefix, order_by, contents, reverse_order):
     </body></html>
     """
 
-    return Environment().from_string(HTML).render(prefix=prefix, contents=sorted_contents, parent_directory=parent_directory, index_link=index_by)
+    return Environment().from_string(HTML).render(prefix=prefix, contents=formatted_contents, parent_directory=parent_directory, index_link=index_by)
 
 def index_file_name(prefix, order_by, reverse_order):
     order_suffix = "_reverse" if reverse_order else ""
@@ -90,32 +104,37 @@ def generate_indexes():
 
 # not in use
 def format_directory_listing(directory_listing):
-    return map(lambda k: {
+    out = []
+    for k in directory_listing:
+        out.append ({
         'name': k['name'],
         'lastModified': format_date(k['lastModified']),
         'size': format_size(k['size'])
-    }, directory_listing)
+    })
+    return out
 
 def walk(path):
     contents = os.listdir(path)
     directory_listing = []
+    errors = []
     # generate indexes for current path
     for file in contents:
         # add size, lastModified, file/folder type to metadata
         fullpath = os.path.join(path, file)
-        print(fullpath)
         if bool(re.match('.*index_by.*\.html$', file)):
             continue
-        directory_listing.append({
-            'name': file,
-            'lastModified': os.path.getmtime(fullpath),
-            'size': os.path.getsize(fullpath)
-        })
+        if(os.path.exists(fullpath)):
+            directory_listing.append({
+                'name': file,
+                'lastModified': os.path.getmtime(fullpath),
+                'size': os.path.getsize(fullpath)
+            })
+        else:
+            print('skipping \'{}\' because the file cannot be read'.format(fullpath))
     for order_by in ['name', 'size', 'lastModified']:
         for reverse_order in [True, False]:
             file_name = os.path.join(path, index_file_name('', order_by, reverse_order))
             rendered_html = render_index(path, order_by, directory_listing, reverse_order)
-            print(directory_listing)
             if args.test:
                 print('Would create: ', file_name)
             else:
