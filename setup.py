98c1b3cb405c4db7af12bd39b43de388160dbba7
Daniel Dreier <d@puppet.com>
2017-05-11 15:27:11 -0700

    bump to rc2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0.rc1',
+    version='0.1.0.rc2',
 
     description='Generate apache-style directory indexes as static HTML files',
     long_description=long_description,
